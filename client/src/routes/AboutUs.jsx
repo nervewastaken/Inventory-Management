5310d13df39c6be8a7d336c1cdbf058a309a6ab9
MUHAMMED SIRFAN C <sirfan.salam@gmail.com>
2024-04-22 22:42:47 +0530

    css
    1.1.0

--- client/src/routes/AboutUs.jsx
+++ client/src/routes/AboutUs.jsx
@@ -1,70 +1,62 @@
 import React from "react";
 import Navbar from "../component/Navbar";
 import ChatbotEmbed from "../component/chatbot";
+import "./AboutUs.css";
+import githubLogo from "./github-logo.png";
+import mailLogo from "./mail-logo.png";
 
 const AboutUs = () => {
   return (
     <div>
       <div className="aboutus">
         <Navbar />
-        <div style={{ textAlign: "center" }}>
+        <div className="contact-container">
           <h2>Contact Us</h2>
-          Krish Verma (22BCE2382) <br></br>
-          krishverma2004@gmail.com<br></br>
-          <br></br>V Hemal Sri (22BDS0432)<br></br>
-          hemal4sri@gmail.com<br></br>
-          <br></br>
-          Muhammed Sirfan (22BKT0147)<br></br>
-          sirfan.salam@gmail.com
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            marginTop: "20px",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              flexDirection: "column",
-              alignItems: "center",
-              padding: "20px",
-              backgroundColor: "rgba(240, 240, 240, 0.5)",
-              border: "1px solid #ccc",
-              borderRadius: "15px",
-              width: "250px",
-            }}
-          >
-            <p>
-              <span>GitHub Link: </span>
-              <a
-                href="https://github.com/nervewastaken/Inventory-Management"
-                style={{
-                  paddingRight: "10px",
-                  color: "#0366d6",
-                  textDecoration: "none",
-                }}
-              >
-                Click here
-              </a>
-            </p>
-
-            <p>
-              <span>Krish's GitHub: </span>
-              <a
-                href="https://github.com/nervewastaken/"
-                style={{
-                  paddingRight: "10px",
-                  color: "#0366d6",
-                  textDecoration: "none",
-                }}
-              >
-                nervewastaken
-              </a>
-            </p>
+          <div className="contact-card">
+            <div className="contact-item">
+              <h3>Krish Verma</h3>
+              <p>22BCE2382</p>
+              <p>
+                <img src={mailLogo} alt="Mail Logo" className="mail-logo" />
+                krishverma2004@gmail.com
+              </p>
+            </div>
+            <div className="contact-item">
+              <h3>V Hemal Sri</h3>
+              <p>22BDS0432</p>
+              <p>
+                <img src={mailLogo} alt="Mail Logo" className="mail-logo" />
+                hemal4sri@gmail.com
+              </p>
+            </div>
+            <div className="contact-item">
+              <h3>Muhammed Sirfan</h3>
+              <p>22BKT0147</p>
+              <p>
+                <img src={mailLogo} alt="Mail Logo" className="mail-logo" />
+                sirfan.salam@gmail.com
+              </p>
+            </div>
+          </div>
+          <div className="links-container">
+            <a
+              href="https://github.com/nervewastaken/Inventory-Management"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link-button"
+            >
+              <img src={githubLogo} alt="GitHub Logo" className="github-logo" />
+              GitHub Link
+            </a>
+            <a
+              href="https://github.com/nervewastaken/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link-button"
+            >
+              <img src={githubLogo} alt="GitHub Logo" className="github-logo" />
+              Krish's GitHub
+            </a>
           </div>
         </div>
       </div>
